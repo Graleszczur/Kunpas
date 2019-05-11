fd5afc88bb6a9950dd09c3a65e1b7e1dda85e9e1
Dawid Ćwiek <dawidcwiek@MacBook-Pro-Dawid.local>
2019-05-11 23:39:06 +0200

    add render pdf

--- api/projects/views.py
+++ api/projects/views.py
@@ -1,14 +1,16 @@
 from django.views.generic import View
 from django.utils import timezone
 from .render import Render
+from projects.models import Graph
 
 
 class Pdf(View):
 
     def get(self, request, id):
+        graph = Graph.objects.get(id=id)
         today = timezone.now()
         params = {
             'today': today,
-            'request': request
+            'url': graph.file.url
         }
         return Render.render('graphs/pdf.html', params)
